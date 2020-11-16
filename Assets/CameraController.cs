65ea5ae37779e536322eb557063c3caed9aa4284
Lumbr <jumiciobi@gmail.com>
2020-11-16 15:17:49 +0000

    jude pls

---
+++ Assets/CameraController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffsetValues;
+    public static float rotateSpeed;
+    public Transform pivot;
+    public float maxViewAngle;
+    public float minViewAngle;
+    public bool invertY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateSpeed = 5f;
+
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = null;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        pivot.transform.position = target.transform.position;
+
+        //get the x position of the mouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        pivot.Rotate(0, horizontal, 0);
+
+        //get the Y position of the mouse and rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+
+        //limit up/down camera rotation
+
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180.0f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, pivot.eulerAngles.y, 0.0f);
+        }
+
+        if (pivot.rotation.eulerAngles.x > 180.0f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360.0f + minViewAngle, pivot.eulerAngles.y, 0.0f);
+        }
+        //move the camera based on the current rotation of the target and the original offset
+        float desiredYAngle = pivot.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody myRB;
+    private Vector3 moveInput;
+    public float speed;
+    public GameObject playerModel;
+    public Transform pivot;
+    public float rotateSpeed;
+    public float jumpForce;
+    public bool isJumping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                myRB.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
+            }
+        }
+    }
+    private void OnCollisionStay(Collider other)
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        moveInput = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveInput = moveInput.normalized * speed;
+        
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            //moveSpeed = moveSpeed + momentumIncrease;
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveInput.x, 0f, moveInput.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+    }
+    void FixedUpdate()
+    {
+
+        myRB.AddForce(moveInput);
+        
+    }
+}
